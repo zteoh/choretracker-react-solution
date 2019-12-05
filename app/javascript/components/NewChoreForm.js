29d7cbdc6d5e3fc01d1684ed3d2ab3b35936cfb1
Zoe Teoh <zteoh@andrew.cmu.edu>
2019-12-04 23:34:36 -0500

    Part 3: Creating the New Chore Form

--- app/javascript/components/NewChoreForm.js
+++ app/javascript/components/NewChoreForm.js
@@ -1,12 +1,71 @@
 import React from "react"
 import PropTypes from "prop-types"
+
 class NewChoreForm extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-      </React.Fragment>
-    );
-  }
+
+	state = {
+		child: this.props.children ? this.props.children[0] : null,
+		task: this.props.tasks ? this.props.tasks[0] : null,
+		due_on: '',
+		completed: false
+	}
+
+	childrenOptions = () => {
+		return this.props.children.map((child, index) => {
+			 return (
+				<option value={index}> {child.first_name} {child.last_name} </option>
+			 )
+		})
+	}
+
+	tasksOptions = () => {
+		return this.props.tasks.map((task, index) => {
+			  return (
+				<option value={index}> {task.name} </option>
+			 )
+		 })
+	  }
+
+	handleChildChange = (event) => {
+		this.setState({child: this.props.children[event.target.value]});
+	}
+
+	handleTaskChange = (event) => {
+		this.setState({task: this.props.tasks[event.target.value]});
+	}
+
+	handleDueOnChange = (event) => {
+		this.setState({due_on: event.target.value});
+	}
+
+	render() {
+		return (
+			 <div>
+				  <form>
+						<label>
+						  Child:
+						  <select onChange={this.handleChildChange}>
+							 { this.childrenOptions() }
+						  </select>
+						</label>
+						<br />
+
+						<label>
+							 Task:
+							 <select onChange={this.handleTaskChange}>
+								{ this.tasksOptions() }
+							 </select>
+						</label>
+						<br />
+
+						<input type="date" name="due_on" onChange={this.handleDueOnChange} />
+						<br />
+
+						<input type="submit" value="Submit" />
+					</form>
+		  </div>
+		)
+	}
 }
 
 export default NewChoreForm
